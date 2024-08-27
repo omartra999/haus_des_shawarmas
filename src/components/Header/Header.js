2668924b9da6115f489b4087524c431d565127bd
Omar <omartra069@gmail.com>
2024-08-27 13:23:58 +0200

    nav and header styling aand animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import images from "../../constants/images";
 import './Header.css'
+import { motion, useAnimation } from "framer-motion"
+
 function Header(){
+    const controls = useAnimation();
+    useEffect(() =>{
+        controls.start({
+            scale: [0.8,1.1,1],
+            opacity: [0,1],
+            trasformation : {duration : 0.6, ease: "easeOut"},
+        })
+    }, [controls]);
+
     return(
         <div className="header" >
             <div className="header_content">    
-            <h1>Shawarma Vibes</h1>
-            <p>Willkommen im Haus des Shawarmas – Wo Geschmack auf Tradition trifft!</p>
+            <motion.h1 animate = {controls}>Shawarma Vibes</motion.h1>
+            <motion.p animate = {controls}>Willkommen im Haus des Shawarmas – Wo Geschmack auf Tradition trifft!</motion.p>
             </div>
         </div>
     )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import images from '../../constants/images'
 import { FaInstagram, FaFacebook } from 'react-icons/fa'
+import { motion, useAnimation } from 'framer-motion'
 import './Navbar.css';
 
 function Navbar(){
     const [scrolling, setScrolling] = useState(false);
+    const controls = useAnimation();
 
     useEffect(() =>{
         const handleScroll = () => {
@@ -14,17 +16,19 @@ function Navbar(){
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+
     return (
-        <div className={`navbar ${scrolling ? 'navbar--scrolled' : ''}`}>
+        <motion.div className={`navbar ${scrolling ? 'navbar--scrolled' : ''}`}
+        animate = {{backgroundColor : scrolling ? '#2d2c2c' : 'transparent'}}>
             <nav className='app__nav'>
                     <li><a href="#">Menü</a></li>
                     <li><a href="#">Kontakt</a></li>
                     <li><a href="#">Über uns</a></li>
                 </nav>
-                <a href = "#Home"><img src={images.logo} class = "logo" alt="Logo" /></a>
+                <motion.a animate = {controls} href = "#Home"><img src={images.logo} class = "logo" alt="Logo" /></motion.a>
                 <a class = "icon" href='#'><FaFacebook /></a>
                 <a class = "icon" href='#'><FaInstagram /></a>
-            </div>
+            </motion.div>
         );
 
 }
