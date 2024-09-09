ab7073157ec8a7186ace1189fff0088502eaa354
Omar <omartra069@gmail.com>
2024-09-09 19:51:55 +0200

    new Menu

--- src/components/MenuImage/MenuImage.js
+++ src/components/MenuImage/MenuImage.js
@@ -5,8 +5,11 @@ import './menuImage.css'
 function MenuImage(){
     return (
         <div className="menu-image">
-            <img src={images.menu01} alt="page01" />
-            <img src={images.menu02} alt="page02" />
+            <img className="page01" src={images.menu01} alt="page01" />
+            <img className="page02" src={images.menu02} alt="page02" />
+            <img className="page03" src={images.menu03} alt="page03" />
+            <img className="page04" src={images.menu04} alt="page04" />
+            <img className="page05" src={images.menu05} alt="page05" />
         </div>
     );
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -7,13 +7,17 @@ import hummus from '../assests/hummus.webp';
 import besondere from '../assests/besondere.webp';
 import taco from '../assests/taco.webp';
 import menu from '../assests/menu.webp';
-import menu01 from '../assests/menu01.webp';
-import menu02 from '../assests/menu02.webp';
+import menu01 from '../assests/Menu01.webp';
+import menu02 from '../assests/Menu02.webp';
+import menu03 from '../assests/Menu03.webp';
+import menu04 from '../assests/Menu04.webp';
+import menu05 from '../assests/Menu05.webp';
+
 
 const images = {
     logo, header,
     salad, sandwish,shawarma, hummus, besondere,
-    taco, menu,menu01,menu02
+    taco, menu,menu01,menu02,menu03,menu04,menu05
  };
 
  export default images;
